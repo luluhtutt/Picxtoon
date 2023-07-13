08a1fdb870a94cb0bf6c5a62ef54d3d6f1cf0485
Joanna <yl797@cornell.edu>
2023-07-13 23:42:27 +0100

    Finish initial cartoonifying
    Co-authored-by: Lulu Htutt <lh543@cornell.edu>

--- toon.py
+++ toon.py
@@ -2,37 +2,32 @@ import numpy as np
 from PIL import Image
 import cv2
 
-# Read and convert a colored image into a floating point array with values between 0 and 1.
-
-
 def imread(filename):
-    image = Image.open(filename)
-    image_rgb = image.convert('RGB')
-    image_data = np.asarray(image_rgb, dtype='uint8')
-    return image_data / 255.
-
-
-def imread_gray(filename):
-    return cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-
-
-def gray_scale(img):
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_gray = 0.2125*img[:, :, 0] + 0.7154*img[:, :, 1] + 0.0721*img[:, :, 2]
-    # img_gray = img.convert('L')
-    return img_gray
-
-
-def gaussianizing(img):
-    return cv2.GaussianBlur(img, (9, 9), 0)
-
-
-def edge_mask(gaussian):
-    return cv2.adaptiveThreshold(gaussian, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9, C=2)
-
+    return cv2.imread(filename)
 
 def otsu(gaussian):
     return cv2.threshold(
         gaussian, 0., 255., cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
+def edge_mask(img):
+    gaussian = cv2.GaussianBlur(img, (9, 9), 0)
+    edge =  cv2.adaptiveThreshold(gaussian, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    return edge
+
+
+def combine(orig, masked_img):
+    orig = orig.astype('uint8')
+    masked_img = masked_img.astype('uint8')
+    bilateral_img = cv2.bilateralFilter(orig, 9, 500, 500)
+    print(bilateral_img.shape)
+    print(masked_img.shape)
+    return cv2.bitwise_and(bilateral_img, bilateral_img, mask=masked_img)
+
+
+def cartoon_full(filename):
+    image = cv2.cvtColor(imread(filename), cv2.COLOR_BGR2RGB)
+    edges = edge_mask(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    return combine(image, edges)
 
 # gaussian --> otsu --> noise (dilating and erosing) --> ... --> watershed
